e156aba6adc2db277367e44dcc8b49b914d02e13
Tom Elliott <tom.elliott@nyu.edu>
2020-04-17 15:32:35 -0500

    complete and working test for iospe, which ensures epidoc stylesheets are in place

--- tests/test_iospe.sh
+++ tests/test_iospe.sh
@@ -6,20 +6,14 @@ failout () {
     exit 1
 }
 
-# validate command-line arguments
-# [ "$#" -eq 1 ] || failout "1 argument required, $# provided"
-# [ -f "$1" ] || failout "$1 is not a valid file path (expected xslt file)"
-
 testpath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 xsltpath="$testpath/../data/epidoc-stylesheets/start-edition.xsl"
 [ -f "$xsltpath" ] || failout "Expected EpiDoc Example Stylesheets at $xsltpath, but did not find them."
-outpath="$testpath/output/iospe"
+outpath="$testpath/output/iospe/"
 mkdir -p "$outpath"
-java -jar bin/claxon-1.0.0-SNAPSHOT-standalone.jar --xsl "$xsltpath" --dir "$testpath/data/xml/iospe/" --out "$outpath" --ext .xhtml leiden-style iospe edn-structure iospe internal-app-style iospe external-app-style iospe 
-#result='Some text here.'
-#if [[ $(< "$testpath/output/basic_tei.txt") != "$result" ]]; then
-#    echo "FAILURE!"
-#else 
-#    echo "SUCCESS!"
-#fi
-# rm -rf "$testpath/output"
+java -jar bin/claxon-1.0.1-SNAPSHOT-standalone.jar --xsl "$xsltpath" --dir "$testpath/data/xml/iospe/" --out "$outpath" --ext .xhtml leiden-style iospe edn-structure iospe internal-app-style iospe external-app-style iospe 
+cd "$outpath"
+sha1sum -c --status < ../../data/html/iospe_1_5.xhtml.sha1
+[ $? -eq 0 ] || failout "FAILURE: Transform of tests/data/xml/iospe/iospe_1_5.xml to XHTML failed (sha1 checksum did not match data/html/iospe/iospe_1_5.xhtml.sha1"
+echo "SUCCESS!"
+rm -rf "$testpath/output"
