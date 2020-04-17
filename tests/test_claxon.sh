c13e491460cb22764b77df4e7f5487e1ff9d252f
Tom Elliott <tom.elliott@nyu.edu>
2020-04-17 15:32:13 -0500

    bump claxon version

--- tests/test_claxon.sh
+++ tests/test_claxon.sh
@@ -3,7 +3,7 @@
 
 testpath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir "$testpath/output"
-java -jar bin/claxon-1.0.0-SNAPSHOT-standalone.jar --xsl "$testpath/data/xsl/basic_tei.xsl" --dir "$testpath/data/xml/basic/" --out "$testpath/output/" --ext .txt
+java -jar bin/claxon-1.0.1-SNAPSHOT-standalone.jar --xsl "$testpath/data/xsl/basic_tei.xsl" --dir "$testpath/data/xml/basic/" --out "$testpath/output/" --ext .txt
 result='Some text here.'
 if [[ $(< "$testpath/output/basic_tei.txt") != "$result" ]]; then
     echo "FAILURE!"
